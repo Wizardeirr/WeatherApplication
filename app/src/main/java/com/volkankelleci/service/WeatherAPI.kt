aa6eb7bce70215a989d98ed8fc35237048c41c21
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-12-12 15:08:52 +0300

    NullPointer problem solved

--- app/src/main/java/com/volkankelleci/service/WeatherAPI.kt
+++ app/src/main/java/com/volkankelleci/service/WeatherAPI.kt
@@ -6,10 +6,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherAPI {
-    @GET("current?access_key=3268eb81ec16e6f1542c79dc3c7bad12&")
+    @GET("current?access_key=29bc518b671677fd2732a2a4bd77eec6&")
     //BASE_URL=http://api.weatherstack.com/current?access_key=3268eb81ec16e6f1542c79dc3c7bad12&query=Malatya
 
-    fun getData(@Query("query")cityName:String):Single<WeatherAppModel>
+    fun getData(@Query("query") cityName:String):Single<WeatherAppModel>
 
     /*
     @GET("api/users")
--- app/src/main/java/com/volkankelleci/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/weatherapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.volkankelleci.weatherapplication
 
+import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
@@ -21,19 +22,19 @@ class MainActivity() : AppCompatActivity() {
         GET=getSharedPreferences(packageName, MODE_PRIVATE)
         SET=GET.edit()
 
-        var countryName=GET.getString("cityName","")
-        city_edit_text.setText(countryName)
-
-
         viewModel = ViewModelProviders.of(this).get(WeatherFragmentViewModel::class.java)
+
+        var countryName=GET.getString("cityName","Malatya")
+        city_edit_text.setText(countryName)
         viewModel.refreshData(countryName!!)
+
         getDataFromInternet()
 
+
         swipeRefreshLayout.setOnRefreshListener {
-            progress_Bar.visibility=View.VISIBLE
+            progress_Bar.visibility=View.GONE
             error_text.visibility=View.GONE
             linearLayout2.visibility=View.GONE
-
 
             var cityName=GET.getString("cityName",countryName)
             city_edit_text.setText(cityName)
@@ -45,10 +46,9 @@ class MainActivity() : AppCompatActivity() {
             val cityName=city_edit_text.text.toString()
             SET.putString("cityName",cityName)
             SET.apply()
-            viewModel.refreshData(cityName!!)
+            viewModel.refreshData(cityName)
             getDataFromInternet()
         }
-        getDataFromInternet()
 
     }
 
@@ -58,15 +58,13 @@ class MainActivity() : AppCompatActivity() {
         viewModel.weatherData.observe(this, Observer {
             it?.let {
                 linearLayout2.visibility = View.VISIBLE
-                hot_view.text = "${it.current.temperature.toString()}°C"
+                hot_view.text = "${it.current.temperature}°C"
                 city_name_text.text = it.location.name
                 country_code.text = it.location.country
                 humidity.text = "Nem % ${it.current.humidity.toString()}"
                 winspeed.text = "Rüzgar Hızı ${it.current.windSpeed.toString()}km/s"
                 precip.text = "Yağmur % ${it.current.precip.toString()}"
                 observation_time.text = "Güncellenme Zamanı ${it.current.observationTime}"
-
-
             }
         })
         viewModel.errorMessage.observe(this, Observer {
